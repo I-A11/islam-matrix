36638319984a9714c1afeff5550919070a2a161a
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-14 19:23:35 +1000

    Start setting app place holders.

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,10 @@
+const Navbar = () => {
+  return (
+    <nav>
+      <div className="">name</div>
+      <div className="">links</div>
+      <div className="">mode</div>
+    </nav>
+  );
+};
+export default Navbar;
