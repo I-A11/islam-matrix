550391372d50282f4f74ddbe9dfdceedd451bc30
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-20 15:24:00 +1000

    Create a second navbar to use it in mobile view.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,14 +1,25 @@
 const Navbar = () => {
   return (
-    <nav className="h-12 w-full bg-cyan-600 ">
-      <div className="flex justify-around items-center ">
+    <nav className="w-full bg-cyan-600 py-8 ">
+      <div className=" justify-around hidden md:flex  ">
         <div className="">Logo</div>
-        <div className="flex justify-between ">
+        <div className="flex justify-between space-x-6 ">
           <div>Home</div>
           <div>About</div>
           <div>Archive</div>
         </div>
         <div className="">mode</div>
+        <div className="">matrix</div>
+      </div>
+      <div className="flex justify-around md:hidden ">
+        <div className="">LogoMobile</div>
+        <div className="flex justify-between space-x-6 ">
+          <div>Home</div>
+          <div>About</div>
+          <div>Archive</div>
+        </div>
+        <div className="">mode</div>
+        <div className="">matrix</div>
       </div>
     </nav>
   );
