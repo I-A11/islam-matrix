2be4f30158b3b041695b233148a0e18f76eb3402
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-28 18:00:53 +1000

    Add underline under links at hover effect.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,12 +5,18 @@ const Navbar = () => {
   return (
     <div>
       <nav className="w-full bg-cyan-100 sticky top-0 shadow-lg">
-        <div className="justify-around items-center hidden md:flex py-6 border-b-1">
-          <Link href="/">Islam Aboamh</Link>
+        <div className="justify-around items-center hidden md:flex py-6 border-b-1 ">
+          <Link className="hover:underline decoration-1" href="/">
+            Islam Aboamh
+          </Link>
           <div className="rotate-90 w-20 border-b-2 border-gray-800"></div>
           <div className="flex justify-around space-x-10 ">
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
+            <Link className="hover:underline decoration-1" href="/">
+              Home
+            </Link>
+            <Link className="hover:underline decoration-1" href="/about">
+              About
+            </Link>
           </div>
           <div className="rotate-90 w-20 border-b-2 border-gray-800 translate-x-5"></div>
           <div className="flex mr-6">
@@ -24,11 +30,17 @@ const Navbar = () => {
         </div>
         {/* Mobile navbar */}
         <div className="container md:hidden  max-w-full h-32 grid grid-cols-[3fr,1fr]">
-          <div className="item-1">Islam Aboamh</div>
+          <Link className="item-1 hover:underline decoration-1" href="/">
+            Islam Aboamh
+          </Link>
           <div className="horizontal border-t-2 border-gray-800"></div>
           <div className="item-2">
-            <Link href="/">Home</Link>
-            <Link href="/about">About</Link>
+            <Link className="hover:underline decoration-1" href="/">
+              Home
+            </Link>
+            <Link className="hover:underline decoration-1" href="/about">
+              About
+            </Link>
           </div>
           <div className="rotate-90 -translate-x-16 -translate-y-[3.6rem] w-32 border-b-2 border-gray-800"></div>
           <div className="item-3 ml-6  md:space-x-0">
