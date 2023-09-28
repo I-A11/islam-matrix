ca2550eded65e5bfbb904b71172b67ae260af860
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-09-28 15:05:17 +1000

    Add links to navbar capsules.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <div>
       <nav className="w-full bg-cyan-100 sticky top-0 shadow-lg">
         <div className="justify-around items-center hidden md:flex py-6 border-b-1">
-          <div className="">Islam Aboamh</div>
+          <Link href="/">Islam Aboamh</Link>
           <div className="rotate-90 w-20 border-b-2 border-gray-800"></div>
           <div className="flex justify-around space-x-10 ">
             <Link href="/">Home</Link>
@@ -14,19 +14,23 @@ const Navbar = () => {
           </div>
           <div className="rotate-90 w-20 border-b-2 border-gray-800 translate-x-5"></div>
           <div className="flex mr-6">
-            <BsCapsule fill="red" size={32} className="text-red-700" />
-            <BsCapsule fill="blue" size={32} className="text-blue-700" />
+            <Link href="/matrix">
+              <BsCapsule fill="red" size={32} className="text-red-700" />
+            </Link>
+            <Link href="/">
+              <BsCapsule fill="blue" size={32} className="text-blue-700" />
+            </Link>
           </div>
         </div>
         {/* Mobile navbar */}
         <div className="container md:hidden  max-w-full h-32 grid grid-cols-[3fr,1fr]">
           <div className="item-1">Islam Aboamh</div>
-          {/* <div className="horizontal border-t-2 border-gray-800"></div> */}
+          <div className="horizontal border-t-2 border-gray-800"></div>
           <div className="item-2">
             <Link href="/">Home</Link>
             <Link href="/about">About</Link>
           </div>
-          {/* <div className="rotate-90 -translate-x-16 -translate-y-[3.6rem] w-32 border-b-2 border-gray-800"></div> */}
+          <div className="rotate-90 -translate-x-16 -translate-y-[3.6rem] w-32 border-b-2 border-gray-800"></div>
           <div className="item-3 ml-6  md:space-x-0">
             <Link href="/matrix">
               <BsCapsule fill="red" size={32} className="text-red-700" />
