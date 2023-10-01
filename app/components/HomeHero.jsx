cd610f66c0da995746a6606cf9cd3d77cc616b87
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-10-01 20:34:54 +1100

    Add Van Gogh portrait.

--- app/components/HomeHero.jsx
+++ app/components/HomeHero.jsx
@@ -3,14 +3,18 @@
 import Canvas from "./WakeUp";
 import { useColorContext } from "../context/color_context";
 import { GiRabbit } from "react-icons/gi";
+import { CgLoadbar } from "react-icons/cg";
 import Link from "next/link";
 import Image from "next/image";
-import StarryNight from "../assets/starry-night.jpeg";
+import Shelf from "../assets/shelf.svg";
+import VanPortrait from "../assets/van-portrait.svg";
 
 const HomeHero = () => {
   const { color, changeColor } = useColorContext();
   return (
-    <div className={` ${color ? "bg-red-300" : "bg-amber-300"} h-72 relative`}>
+    <div
+      className={` ${color ? "bg-indigo-300" : "bg-amber-300"} h-72 relative`}
+    >
       <div>
         <Canvas />
         <div className="flex justify-center mx-auto -mt-4">
@@ -18,6 +22,18 @@ const HomeHero = () => {
             <GiRabbit size={64} fill="white" />
           </Link>
         </div>
+
+        <div className="mx-auto max-w-[40rem] -mt-20">
+          <Image
+            src={VanPortrait}
+            alt="Van Gogh"
+            width={96}
+            className="rounded-3xl"
+          />
+        </div>
+        {/* <div className="mx-auto max-w-[40rem] -translate-y-28  -translate-x-24">
+          <Image src={Shelf} alt="book shelf" width={420} />
+        </div> */}
       </div>
     </div>
   );
