d3fbe28f5123442cfca6edc0674b610fa8e0bd04
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-10-02 15:56:39 +1100

    Create paintings to add it in home hero section.

--- app/components/HomeHero.jsx
+++ app/components/HomeHero.jsx
@@ -3,38 +3,37 @@
 import Canvas from "./WakeUp";
 import { useColorContext } from "../context/color_context";
 import { GiRabbit } from "react-icons/gi";
-import { CgLoadbar } from "react-icons/cg";
+import { MdLinkedCamera } from "react-icons/md";
 import Link from "next/link";
 import Image from "next/image";
-import Shelf from "../assets/shelf.svg";
-import VanPortrait from "../assets/van-portrait.svg";
+import Shelf from "../assets/images/shelf.svg";
+import VanPortrait from "../assets/images/van-portrait.svg";
+import Painting from "../assets/images/painting.png";
 
 const HomeHero = () => {
   const { color, changeColor } = useColorContext();
   return (
-    <div
-      className={` ${color ? "bg-indigo-300" : "bg-amber-300"} h-72 relative`}
-    >
-      <div>
-        <Canvas />
-        <div className="flex justify-center mx-auto -mt-4">
+    <div className={` ${color ? "bg-indigo-300" : "bg-amber-300"} h-72`}>
+      <Canvas />
+      <div
+        className=" -mt-4 grid grid-cols-9 grid-rows-2 max-w-[50rem] mx-auto"
+        ml-4
+      >
+        <div className="row-start-1 col-start-1 col-end-3">
+          <Image src={Painting} alt="Van Gogh" width={400} className="ml-6" />
+        </div>
+        <div className=" row-start-1 col-start-5 col-span-2">
           <Link href="/matrix">
-            <GiRabbit size={64} fill="white" />
+            <GiRabbit size={72} fill="white" />
           </Link>
         </div>
 
-        <div className="mx-auto max-w-[40rem] -mt-20">
-          <Image
-            src={VanPortrait}
-            alt="Van Gogh"
-            width={96}
-            className="rounded-3xl"
-          />
+        <div className="col-start-1 col-end-3">
+          {/* <Image src={Shelf} alt="book shelf" width={480} /> */}
         </div>
-        {/* <div className="mx-auto max-w-[40rem] -translate-y-28  -translate-x-24">
-          <Image src={Shelf} alt="book shelf" width={420} />
-        </div> */}
       </div>
+
+      {/* //////////////// */}
     </div>
   );
 };
