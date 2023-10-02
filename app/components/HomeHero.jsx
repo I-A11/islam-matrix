4572640e98189d53aa4d16ddba1eb8c0fccb8ae9
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-10-02 18:57:24 +1100

    Add media part at the home hero section.

--- app/components/HomeHero.jsx
+++ app/components/HomeHero.jsx
@@ -6,31 +6,31 @@ import { GiRabbit } from "react-icons/gi";
 import { MdLinkedCamera } from "react-icons/md";
 import Link from "next/link";
 import Image from "next/image";
-import Shelf from "../assets/images/shelf.svg";
+import Media from "../assets/images/media.png";
 import VanPortrait from "../assets/images/van-portrait.svg";
 import Painting from "../assets/images/paint-1.png";
 
 const HomeHero = () => {
   const { color, changeColor } = useColorContext();
   return (
-    <div className={` ${color ? "bg-indigo-300" : "bg-amber-300"} h-72`}>
+    <div
+      className={` ${color ? "bg-indigo-300" : "bg-amber-300"} h-56 xs:h-80`}
+    >
       <Canvas />
-      <div className=" -mt-4 grid grid-cols-11  max-w-[50rem] mx-auto" ml-4>
-        <div className=" col-start-1 col-end-4">
-          <Image src={Painting} alt="Van Gogh" width={400} className="ml-6" />
+      <div className=" -mt-8 grid xs:grid-cols-11 grid-cols-3 max-w-[50rem] mx-auto">
+        <div className=" col-start-1 col-end-5 hidden xs:block">
+          <Image src={Painting} alt="Paintings" width={250} className="ml-6" />
         </div>
-        <div className="  col-start-6 col-span-2">
+        <div className="xs:col-start-6 xs:col-end-7 col-start-2 col-end-3">
           <Link href="/matrix">
-            <GiRabbit size={72} fill="white" />
+            <GiRabbit size={72} fill="white" className="ml-4 xs:ml-0" />
           </Link>
         </div>
 
-        <div className="col-start-1 col-end-3">
-          {/* <Image src={Shelf} alt="book shelf" width={480} /> */}
+        <div className="col-start-8 col-end-12 self-end hidden xs:block">
+          <Image src={Media} alt="media" width={600} />
         </div>
       </div>
-
-      {/* //////////////// */}
     </div>
   );
 };
