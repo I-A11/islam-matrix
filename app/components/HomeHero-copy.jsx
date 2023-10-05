13607ed8752fea80dddfdc587643dd61f1342751
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-10-05 16:26:53 +1100

    Create home hero-copy component to start creating  new features.

---
+++ app/components/HomeHero-copy.jsx
@@ -0,0 +1,47 @@
+"use client";
+
+import Canvas from "./WakeUp";
+import { useColorContext } from "../context/color_context";
+import { GiRabbit } from "react-icons/gi";
+import Link from "next/link";
+import Image from "next/image";
+import Media from "../assets/images/media.png";
+import MediaColored from "../assets/images/media-1.png";
+import Painting from "../assets/images/paint-1.png";
+import Desktop from "../assets/images/desktop.jpeg";
+
+const HomeHero = () => {
+  const { color } = useColorContext();
+  return (
+    <div
+      className={` ${color ? "bg-indigo-300" : "bg-amber-300"} h-56 xs:h-80`}
+    >
+      <div>
+        <Image src={Desktop} alt="desktop" />
+      </div>
+      <div className="flex xs:flex-row flex-col justify-center xs:pr-8 pr-0 items-center xs:max-w-[36rem] mx-auto ">
+        <Canvas />
+        <div className="">
+          <Link href="/matrix">
+            <GiRabbit size={60} fill="white" className="" />
+          </Link>
+        </div>
+      </div>
+
+      <div className=" -mt-8 grid xs:grid-cols-11 grid-cols-3 max-w-[50rem] mx-auto">
+        <div className=" col-start-3 col-end-6 hidden xs:block">
+          <Image src={Painting} alt="Paintings" width={250} className="ml-6" />
+        </div>
+
+        <div className="col-start-7 col-end-10 self-end hidden xs:block">
+          {color ? (
+            <Image src={Media} alt="media" width={600} />
+          ) : (
+            <Image src={MediaColored} alt="media" width={600} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default HomeHero;
--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import HomeHero from "./components/HomeHero";
+import HomeHeroCopy from "./components/HomeHero-copy";
 
 export default function Home() {
   return (
     <>
       <Navbar />
-      <HomeHero />
+      {/* <HomeHero /> */}
+      <HomeHeroCopy />
     </>
   );
 }
