be752164157e09d2919019e83343f8047fed5137
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-10-06 15:51:33 +1100

    Add desktop image to use it in hero section.

--- app/components/HomeHero-copy.jsx
+++ app/components/HomeHero-copy.jsx
@@ -8,7 +8,6 @@ import Image from "next/image";
 import Media from "../assets/images/media.png";
 import MediaColored from "../assets/images/media-1.png";
 import Painting from "../assets/images/paint-1.png";
-import Desktop from "../assets/images/desktop.jpeg";
 
 const HomeHero = () => {
   const { color } = useColorContext();
@@ -16,9 +15,6 @@ const HomeHero = () => {
     <div
       className={` ${color ? "bg-indigo-300" : "bg-amber-300"} h-56 xs:h-80`}
     >
-      <div>
-        <Image src={Desktop} alt="desktop" />
-      </div>
       <div className="flex xs:flex-row flex-col justify-center xs:pr-8 pr-0 items-center xs:max-w-[36rem] mx-auto ">
         <Canvas />
         <div className="">
@@ -29,11 +25,16 @@ const HomeHero = () => {
       </div>
 
       <div className=" -mt-8 grid xs:grid-cols-11 grid-cols-3 max-w-[50rem] mx-auto">
-        <div className=" col-start-3 col-end-6 hidden xs:block">
+        <div className=" col-start-1 col-end-5 hidden xs:block">
           <Image src={Painting} alt="Paintings" width={250} className="ml-6" />
         </div>
+        {/* <div className="xs:col-start-6 xs:col-end-7 col-start-2 col-end-3">
+          <Link href="/matrix">
+            <GiRabbit size={72} fill="white" className="ml-4 xs:ml-0" />
+          </Link>
+        </div> */}
 
-        <div className="col-start-7 col-end-10 self-end hidden xs:block">
+        <div className="col-start-8 col-end-12 self-end hidden xs:block">
           {color ? (
             <Image src={Media} alt="media" width={600} />
           ) : (
