2530482328362def932255784684f08d742e6cbd
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-10-25 18:36:40 +1100

    Add background and text to test matrix page.

--- app/matrix/page.jsx
+++ app/matrix/page.jsx
@@ -1,4 +1,13 @@
+import Link from "next/link";
+
 const Matrix = () => {
-  return <div>Matrix page</div>;
+  return (
+    <div className="h-[100vh] z-50 bg-slate-600">
+      <Link href="/">Home</Link>
+      <div className="">
+        <div>Matrix page</div>
+      </div>
+    </div>
+  );
 };
 export default Matrix;
