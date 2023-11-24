b5a57c9431f12963c772c41faa9635ddbadfba2a
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-11-24 19:12:37 +1100

    Update projects data.

--- app/components/projects-section/ProjectsData.jsx
+++ app/components/projects-section/ProjectsData.jsx
@@ -1,53 +1,68 @@
-import CoinImage from "../../assets/images/media-1.png";
+import HomeStyle from "../../assets/images/projectsImages/home-style.PNG";
+import MovieStudio from "../../assets/images/projectsImages/movie-studio.PNG";
+import Apei from "../../assets/images/projectsImages/apei.png";
+import CryptoGalaxy from "../../assets/images/projectsImages/crypto-galaxy.PNG";
+import TaskHunter from "../../assets/images/projectsImages/taskhunter-1.PNG";
+import Tetris from "../../assets/images/projectsImages/tetris.png";
+import MoviesClub from "../../assets/images/projectsImages/Movies-club.PNG";
+import SportSociety from "../../assets/images/projectsImages/sport-society.PNG";
 
 export const ProjectData = [
   {
     id: 1,
-    projectTitle: "Coin website",
-    projectImage: CoinImage,
-    siteLink: "https://crypto-galaxy-drab.vercel.app/",
-    githubLink: "https://github.com/I-A11/CryptoGalaxy",
+    projectTitle: "Home Style",
+    projectImage: HomeStyle,
+    siteLink: "https://home-style.netlify.app/",
+    githubLink: "https://github.com/I-A11/home-style",
   },
   {
     id: 2,
-    projectTitle: "Coin website",
-    projectImage: CoinImage,
-    siteLink: "https://crypto-galaxy-drab.vercel.app/",
-    githubLink: "https://github.com/I-A11/CryptoGalaxy",
+    projectTitle: "Movie Studio",
+    projectImage: MovieStudio,
+    siteLink: "https://studio-movies.netlify.app/",
+    githubLink: "https://github.com/I-A11/Movie-Studio",
   },
   {
     id: 3,
-    projectTitle: "Coin website",
-    projectImage: CoinImage,
-    siteLink: "https://crypto-galaxy-drab.vercel.app/",
-    githubLink: "https://github.com/I-A11/CryptoGalaxy",
+    projectTitle: "APEI",
+    projectImage: Apei,
+    siteLink: "https://charming-bubblegum-a0b061.netlify.app/",
+    githubLink: "https://github.com/I-A11/apei",
   },
   {
     id: 4,
-    projectTitle: "Coin website",
-    projectImage: CoinImage,
+    projectTitle: "Crypto Galaxy",
+    projectImage: CryptoGalaxy,
     siteLink: "https://crypto-galaxy-drab.vercel.app/",
     githubLink: "https://github.com/I-A11/CryptoGalaxy",
   },
   {
     id: 5,
-    projectTitle: "Coin website",
-    projectImage: CoinImage,
-    siteLink: "https://crypto-galaxy-drab.vercel.app/",
-    githubLink: "https://github.com/I-A11/CryptoGalaxy",
+    projectTitle: "Task Hunter",
+    projectImage: TaskHunter,
+    siteLink: "https://task-hunter.netlify.app/",
+    githubLink: "https://github.com/I-A11/Task-Hunter",
   },
   {
     id: 6,
-    projectTitle: "Coin website",
-    projectImage: CoinImage,
-    siteLink: "https://crypto-galaxy-drab.vercel.app/",
-    githubLink: "https://github.com/I-A11/CryptoGalaxy",
+    projectTitle: "Tetris",
+    projectImage: Tetris,
+    siteLink: "https://quizzical-ptolemy-8ea29d.netlify.app/",
+    githubLink: "https://github.com/I-A11/Tetris-Game",
   },
   {
     id: 7,
-    projectTitle: "Coin website",
-    projectImage: CoinImage,
-    siteLink: "https://crypto-galaxy-drab.vercel.app/",
-    githubLink: "https://github.com/I-A11/CryptoGalaxy",
+    projectTitle: "Movies Club",
+    projectImage: MoviesClub,
+    siteLink: "https://moviesclub.netlify.app/",
+    githubLink: "https://github.com/I-A11/MoviesClub",
+  },
+
+  {
+    id: 8,
+    projectTitle: "Sport Society",
+    projectImage: SportSociety,
+    siteLink: "https://sportsociety.netlify.app/",
+    githubLink: "https://github.com/I-A11/Sport-Society",
   },
 ];
