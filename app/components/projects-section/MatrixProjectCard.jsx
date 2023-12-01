8191d930f096d9fa4c99fb976fdf751de1a30c04
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-12-01 17:19:14 +1100

    Create matrix rain in matrix page.

---
+++ app/components/projects-section/MatrixProjectCard.jsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { motion } from "framer-motion";
+import { useState } from "react";
+
+const hiddenMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 30px, rgba(0,0,0,1) 30px, rgba(0,0,0,1) 30px)`;
+const visibleMask = `repeating-linear-gradient(to right, rgba(0,0,0,0) 0px, rgba(0,0,0,0) 0px, rgba(0,0,0,1) 0px, rgba(0,0,0,1) 30px)`;
+
+const ProjectCard = ({ image, title, siteLink, githubLink }) => {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isInView, setIsInView] = useState(false);
+
+  return (
+    <motion.div
+      initial={false}
+      animate={
+        isLoaded && isInView
+          ? { WebkitMaskImage: visibleMask, maskImage: visibleMask }
+          : { WebkitMaskImage: hiddenMask, maskImage: hiddenMask }
+      }
+      transition={{ duration: 1 }}
+      viewport={{ once: true }}
+      onViewportEnter={() => setIsInView(true)}
+      className="flex flex-col justify-center"
+    >
+      <div className="h-64 overflow-hidden">
+        <Image
+          className="rounded-xl overflow-hidden"
+          style={{ width: "100%", height: "100%", filter: "grayScale(100%)" }}
+          alt={title}
+          src={image}
+          onLoad={() => setIsLoaded(true)}
+        />
+      </div>
+      <h3 className="mt-6 text-center font-bold">{title}</h3>
+      <div className="flex justify-center items-center space-x-5 mt-2 text-sm">
+        <a className="bg-white p-2 rounded-md" href={siteLink} target="_blank">
+          View Project
+        </a>
+        <a
+          className="bg-white p-2 rounded-md"
+          href={githubLink}
+          target="_blank"
+        >
+          Project Code
+        </a>
+      </div>
+    </motion.div>
+  );
+};
+export default ProjectCard;
--- app/matrix/page.jsx
+++ app/matrix/page.jsx
@@ -4,11 +4,32 @@ import Link from "next/link";
 import Navbar from "../components/common/matrix/Navbar";
 import Footer from "../components/common/matrix/Footer";
 import MatrixRain from "./MatrixRain";
+import MatrixProjectCard from "../components/projects-section/MatrixProjectCard";
+import ProjectData from "../components/projects-section/ProjectsData";
 
 const Matrix = () => {
   return (
     <div className="">
       <Navbar />
+      <div className=" bg-black">
+        <div className="section grid py-10 mx-auto pt-14 px-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+          {ProjectData.map((project) => {
+            const { id, projectTitle, projectImage, siteLink, githubLink } =
+              project;
+            return (
+              <div className="mt-10 md:px-0 sm:px-20 px-0" key={id}>
+                <MatrixProjectCard
+                  style={{ filter: "grayscale(100%)" }}
+                  image={projectImage}
+                  title={projectTitle}
+                  siteLink={siteLink}
+                  githubLink={githubLink}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
       <MatrixRain />
       <Footer />
     </div>
