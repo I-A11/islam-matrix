a7ee5b1764f390eb01d142f2df8dc220a4ff2154
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2024-08-08 10:08:10 +1000

    Add priceless chemist project card.

--- app/components/projects-section/ProjectsData.jsx
+++ app/components/projects-section/ProjectsData.jsx
@@ -6,52 +6,60 @@ import TaskHunter from "../../assets/images/projectsImages/taskhunter-1.PNG";
 import Tetris from "../../assets/images/projectsImages/tetris.png";
 import MoviesClub from "../../assets/images/projectsImages/Movies-club.PNG";
 import SportSociety from "../../assets/images/projectsImages/sport-society.PNG";
+import PricelessChemist from "../../assets/images/projectsImages/priceless-chemist.png";
 
 const ProjectData = [
   {
     id: 1,
+    projectTitle: "Priceless Chemist",
+    projectImage: PricelessChemist,
+    siteLink: "https://pricelesschemist.com.au/",
+    githubLink: "https://github.com/I-A11/priceless-chemist",
+  },
+  {
+    id: 2,
     projectTitle: "Home Style",
     projectImage: HomeStyle,
     siteLink: "https://home-style.netlify.app/",
     githubLink: "https://github.com/I-A11/home-style",
   },
   {
-    id: 2,
+    id: 3,
     projectTitle: "Movie Studio",
     projectImage: MovieStudio,
     siteLink: "https://studio-movies.netlify.app/",
     githubLink: "https://github.com/I-A11/Movie-Studio",
   },
   {
-    id: 3,
+    id: 4,
     projectTitle: "APEI",
     projectImage: Apei,
     siteLink: "https://charming-bubblegum-a0b061.netlify.app/",
     githubLink: "https://github.com/I-A11/apei",
   },
   {
-    id: 4,
+    id: 5,
     projectTitle: "Crypto Galaxy",
     projectImage: CryptoGalaxy,
     siteLink: "https://crypto-galaxy-drab.vercel.app/",
     githubLink: "https://github.com/I-A11/CryptoGalaxy",
   },
   {
-    id: 5,
+    id: 6,
     projectTitle: "Task Hunter",
     projectImage: TaskHunter,
     siteLink: "https://task-hunter.netlify.app/",
     githubLink: "https://github.com/I-A11/Task-Hunter",
   },
   {
-    id: 6,
+    id: 7,
     projectTitle: "Tetris",
     projectImage: Tetris,
     siteLink: "https://quizzical-ptolemy-8ea29d.netlify.app/",
     githubLink: "https://github.com/I-A11/Tetris-Game",
   },
   {
-    id: 7,
+    id: 8,
     projectTitle: "Movies Club",
     projectImage: MoviesClub,
     siteLink: "https://moviesclub.netlify.app/",
@@ -59,7 +67,7 @@ const ProjectData = [
   },
 
   {
-    id: 8,
+    id: 9,
     projectTitle: "Sport Society",
     projectImage: SportSociety,
     siteLink: "https://sportsociety.netlify.app/",
