920245d4b67c2b80e4e96e28704289cc1719ee51
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2025-07-14 11:14:21 +1000

    Add the compounding warehouse project.

--- app/components/projects-section/ProjectsData.jsx
+++ app/components/projects-section/ProjectsData.jsx
@@ -1,31 +1,31 @@
 import HomeStyle from "../../assets/images/projectsImages/home-style.PNG";
 import MovieStudio from "../../assets/images/projectsImages/movie-studio.PNG";
-import Apei from "../../assets/images/projectsImages/apei.png";
+import CompoundingWarehouse from "../../assets/images/projectsImages/compounding-warehouse.png";
 import CryptoGalaxy from "../../assets/images/projectsImages/CryptoGalaxy.png";
 import TaskHunter from "../../assets/images/projectsImages/taskhunter-1.PNG";
 import Tetris from "../../assets/images/projectsImages/tetris.png";
 import MoviesClub from "../../assets/images/projectsImages/Movies-club.PNG";
-import SportSociety from "../../assets/images/projectsImages/sport-society.PNG";
-import PricelessChemist from "../../assets/images/projectsImages/priceless-chemist.png";
+
 import Fts from "../../assets/images/projectsImages/fts.png";
 
 const ProjectData = [
   {
     id: 1,
+    projectTitle: "The Compounding Warehouse",
+    projectImage: CompoundingWarehouse,
+    siteLink: "https://thecompoundingwarehouse.com.au/",
+    githubLink: "https://github.com/I-A11/the-compounding-warehouse",
+    showCodeButton: false,
+  },
+  {
+    id: 2,
     projectTitle: "Fintacs Taxation",
     projectImage: Fts,
     siteLink: "https://fintacstax.com.au/",
     githubLink: "https://github.com/I-A11/fts",
     showCodeButton: false,
   },
-  // {
-  //   id: 2,
-  //   projectTitle: "Priceless Chemist",
-  //   projectImage: PricelessChemist,
-  //   siteLink: "https://pricelesschemist.com.au/",
-  //   githubLink: "https://github.com/I-A11/priceless-chemist",
-  //   showCodeButton: false,
-  // },
+
   {
     id: 3,
     projectTitle: "Home Style",
@@ -42,14 +42,14 @@ const ProjectData = [
     githubLink: "https://github.com/I-A11/Movie-Studio",
     showCodeButton: true,
   },
-  {
-    id: 5,
-    projectTitle: "APEI",
-    projectImage: Apei,
-    siteLink: "https://charming-bubblegum-a0b061.netlify.app/",
-    githubLink: "https://github.com/I-A11/apei",
-    showCodeButton: false,
-  },
+  // {
+  //   id: 5,
+  //   projectTitle: "APEI",
+  //   projectImage: Apei,
+  //   siteLink: "https://charming-bubblegum-a0b061.netlify.app/",
+  //   githubLink: "https://github.com/I-A11/apei",
+  //   showCodeButton: false,
+  // },
   {
     id: 6,
     projectTitle: "Crypto Galaxy",
@@ -83,15 +83,6 @@ const ProjectData = [
     githubLink: "https://github.com/I-A11/MoviesClub",
     showCodeButton: true,
   },
-
-  // {
-  //   id: 10,
-  //   projectTitle: "Sport Society",
-  //   projectImage: SportSociety,
-  //   siteLink: "https://sportsociety.netlify.app/",
-  //   githubLink: "https://github.com/I-A11/Sport-Society",
-  //   showCodeButton: true,
-  // },
 ];
 
 export default ProjectData;
